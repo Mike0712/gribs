815c3cae375da6a5e23adbb327cf7a71768298fc
mike <barrister2003@gmail.com>
2017-09-24 18:53:25 +0300

    #first_page

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>
+            INDEX
+        </p>
+        <router-link tag="a" to="/vuejs"><h3>ВХОД НА САЙТ</h3></router-link>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
